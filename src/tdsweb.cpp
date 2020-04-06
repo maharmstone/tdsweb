5621e69da0e57dbaeabccabb568f58b7883eeb8e
Mark Harmstone <mark@harmstone.com>
2020-04-06 11:14:31 +0100

    handle logging in

--- src/tdsweb.cpp
+++ src/tdsweb.cpp
@@ -9,6 +9,7 @@ using namespace std;
 using json = nlohmann::json;
 
 static const string DB_SERVER = "luthien"; // FIXME - move to command line
+static const string DB_APP = "tdsweb";
 static const uint16_t PORT = 52441; // FIXME - move to command line?
 static const unsigned int BACKLOG = 10;
 
@@ -21,6 +22,43 @@ static void send_error(ws::client_thread& ct, const string& msg) {
     ct.send(j.dump());
 }
 
+class client {
+public:
+    client(ws::client_thread& ct) : ct(ct) { }
+
+    ~client() {
+        if (tds)
+            delete tds;
+    }
+
+    void login(const json& j);
+
+    ws::client_thread& ct;
+    tds::Conn* tds = nullptr;
+};
+
+void client::login(const json& j) {
+    if (j.count("username") == 0)
+        throw runtime_error("Username not provided.");
+
+    if (j.count("password") == 0)
+        throw runtime_error("Password not provided.");
+
+    if (tds)
+        delete tds;
+
+    // FIXME - message handlers
+
+    tds = new tds::Conn(DB_SERVER, j["username"], j["password"], DB_APP);
+
+    ct.send(json{
+        {"type", "login"},
+        {"success", true},
+        {"server", DB_SERVER},
+        {"username", j["username"]}
+    }.dump());
+}
+
 static void msg_handler(ws::client_thread& ct, const string& msg) {
     try {
         json j = json::parse(msg);
@@ -28,12 +66,13 @@ static void msg_handler(ws::client_thread& ct, const string& msg) {
         if (j.count("type") == 0)
             throw runtime_error("No message type given.");
 
+        auto c = (client*)ct.context;
+
         string type = j["type"];
 
-        if (type == "login") {
-            // FIXME
-            throw runtime_error(u8"¯\\_(ツ)_/¯");
-        } else
+        if (type == "login")
+            c->login(j);
+        else
             throw runtime_error("Unrecognized message type \"" + type + "\".");
     } catch (const exception& e) {
         send_error(ct, e.what());
@@ -41,13 +80,15 @@ static void msg_handler(ws::client_thread& ct, const string& msg) {
 }
 
 static void conn_handler(ws::client_thread& ct) {
-    cout << "Client connected." << endl;
-    // FIXME
+    ct.context = new client(ct);
 }
 
 static void disconn_handler(ws::client_thread& ct) {
-    cout << "Client disconnected." << endl;
-    // FIXME
+    if (ct.context) {
+        auto c = (client*)ct.context;
+
+        delete c;
+    }
 }
 
 static void init() {
